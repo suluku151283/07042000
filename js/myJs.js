0d63cf60eb8cc1c9de25e5e4b6c662fca7fdc556
suluku151283 <55490931+suluku151283@users.noreply.github.com>
2021-12-05 17:23:07 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -73,7 +73,7 @@ $('#no').click(() => {
 // generate text in input
 function textGenerate() {
     var n = "";
-    var text = " Tại vì cậu cuteeeeeee hehe :>>>>>>>>> ";
+    var text = " Tại vì cậu đạp trai quá hehe :>>>>>>>>> ";
     var a = Array.from(text);
     var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
     var count = textVal.length;
